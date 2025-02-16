af795d990423ed9851431588c930d43580209339
Mohammed CHBANI <mohamedachbani5@gmail.com>
2025-02-16 19:07:38 +0100

    more fixes

--- src/main/java/com/multiplatform/time_management_backend/academicclass/modal/dto/AcademicClassDto.java
+++ src/main/java/com/multiplatform/time_management_backend/academicclass/modal/dto/AcademicClassDto.java
@@ -6,8 +6,11 @@ import jakarta.validation.constraints.NotNull;
 public record AcademicClassDto(
         @NotNull Long id,
         @NotNull Long semesterId,
+        @NotNull String groupName,
         @NotNull Long groupId,
+        @NotNull String teacherName,
         @NotNull Long teacherId,
+        @NotNull String courseName,
         @NotNull Long courseId
 ) {
 }
